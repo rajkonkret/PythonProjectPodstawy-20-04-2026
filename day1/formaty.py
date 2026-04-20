80c975346573ab0d153afb483c74ba0d5fb47a1e
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 14:35:14 +0200

    [1 25] update formaty.py

--- day1/formaty.py
+++ day1/formaty.py
@@ -52,3 +52,7 @@ print(f'Nasza duża liczba: {liczba:,}')  # Nasza duża liczba: 7,891,234,567,65
 print(f'Nasza duża liczba: {liczba:_}')  # Nasza duża liczba: 7_891_234_567_654_321_890
 print(f'Nasza duża liczba: {liczba:_}'.replace("_", " "))  # Nasza duża liczba: 7 891 234 567 654 321 890
 print(f'Nasza duża liczba: {liczba:_}'.replace("_", "."))  # Nasza duża liczba: 7.891.234.567.654.321.890
+
+liczba = 1_500_000_000_000
+print(liczba)  # 1500000000000
+print(type(liczba))  # <class 'int'>
