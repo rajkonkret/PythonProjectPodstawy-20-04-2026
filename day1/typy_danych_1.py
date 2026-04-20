fcd0df960a99ab0f7fb80b3d3818805bca6523f2
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 15:10:17 +0200

    [1 24] update typy_danych_1.py.py

--- day1/typy_danych_1.py
+++ day1/typy_danych_1.py
@@ -70,3 +70,31 @@ print(bool(""))  # False
 print(bool("0"))  # True
 
 print(bool(None))  # odpowiednik null, stan nieokreślony, nie wiem
+
+# operacje logiczne
+
+print(40 * "-")
+
+# and - i
+print(False and True)  # False
+print(False and False)  # False
+print(True and False)  # False
+print(True and True)  # True
+# ----------------------------------------
+# False
+# False
+# False
+# True
+
+print(30 * "-")
+
+# or - lub
+print(False or False)  # False
+print(False or True)  # True
+print(True or False)  # True
+print(True or True)  # True
+
+# not - negacja
+print(30 * "-")
+print(not True)  # False
+print(not False)  # True
