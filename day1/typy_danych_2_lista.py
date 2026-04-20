a724266025c4f9a1d82fd5743e6ed39a94c67ef2
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 15:22:34 +0200

    [1 27] update typy_danych_2_lista.py.py.py

--- day1/typy_danych_2_lista.py
+++ day1/typy_danych_2_lista.py
@@ -5,9 +5,19 @@
 
 # pusta lista
 lista = []
-print(lista) # []
-print(type(lista)) # <class 'list'>
+print(lista)  # []
+print(type(lista))  # <class 'list'>
 
 pusta_lista = list()
-print(pusta_lista) # []
-print(type(pusta_lista)) # <class 'list'>
+print(pusta_lista)  # []
+print(type(pusta_lista))  # <class 'list'>
+
+# dodawanie elementów do listy
+lista.append("Radek")
+lista.append("Tomek")
+lista.append("Marek")
+lista.append("Sebastian")
+lista.append("Anna")
+lista.append("Magda")
+print(lista)
+# ['Radek', 'Tomek', 'Marek', 'Sebastian', 'Anna', 'Magda']
