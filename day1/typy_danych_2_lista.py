f27344b0c22e80504a4b2f2fab15d10deeb72cde
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 15:20:52 +0200

    [1 26] update typy_danych_2_lista.py.py.py

---
+++ day1/typy_danych_2_lista.py
@@ -0,0 +1,13 @@
+# kolekcje
+
+# lista - przechowuje dowolną ilośc danych, różnego typu na raz
+# zachowuje kolejnośc podczas dodawania elementów
+
+# pusta lista
+lista = []
+print(lista) # []
+print(type(lista)) # <class 'list'>
+
+pusta_lista = list()
+print(pusta_lista) # []
+print(type(pusta_lista)) # <class 'list'>
