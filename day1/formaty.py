68a810a4b28c729143c1c2a3e10ca0096c8dbfd7
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 14:23:39 +0200

    [1 21] update formaty.py

--- day1/formaty.py
+++ day1/formaty.py
@@ -16,3 +16,23 @@ print("Witaj %s, masz teraz %d lat." % (user, wiek))
 # Witaj Tomek, masz teraz 39 lat.
 print(f"Witaj {user}, masz teraz {wiek} lat.")
 # Witaj Tomek, masz teraz 39 lat.
+
+# %i - int
+# %f - float
+
+print("Używamy wersji Pythona %i" % 3)  # Używamy wersji Pythona 3
+print("Używamy wersji Pythona %f" % 3)  # Używamy wersji Pythona 3.000000
+print("Używamy wersji Pythona %.2f" % 3.9)  # Używamy wersji Pythona 3.90
+print("Używamy wersji Pythona %.1f" % 3.9)  # Używamy wersji Pythona 3.9
+print("Używamy wersji Pythona %.0f" % 3.9)  # Używamy wersji Pythona 4 zaorągla
+print("Używamy wersji Pythona %.0f" % 3.5)  # Używamy wersji Pythona 4 zaorągla
+print("Używamy wersji Pythona %.f" % 3.5)  # Używamy wersji Pythona 4 zaorągla
+
+x = 3.8769
+print(x)  # 3.8769
+y = round(x)
+print(y)  # 4
+
+z = round(x, 2)
+print(f"{z=}")  # z=3.88
+print(type(z))  # <class 'float'>
