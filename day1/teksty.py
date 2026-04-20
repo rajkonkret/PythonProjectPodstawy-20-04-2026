c9eb048ff09d3b583b237f5e12cf54d7860c860c
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 13:42:44 +0200

    [1 11] update teksty.py

--- day1/teksty.py
+++ day1/teksty.py
@@ -40,3 +40,14 @@ print(tekst.removesuffix("Świecie"))  # "Witaj "
 
 # strip() - usunięcie białych znaków, wiodących i kończących spacji
 print(tekst.removesuffix("Świecie").strip())  # "Witaj"
+
+encode_s = tekst.encode('utf-8')
+print(encode_s)  # b'Witaj \xc5\x9awiecie'
+print(type(encode_s))  # <class 'bytes'>, typ bajtowy
+# b - bajty
+#  \xc5\x9a - Ś w systemie szesnastkowym
+# \x - dane w systemie szesnastkowym
+
+print(encode_s.decode("utf-8"))  # Witaj Świecie
+
+
