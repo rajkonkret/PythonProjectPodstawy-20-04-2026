8878d1b9502ea0db7fd0d8ab1839bf1f722c1389
rajkonkret <rajkonkret660@gmail.com>
2026-04-20 12:01:21 +0200

    [1 2] update pierwszy.py

---
+++ day1/pierwszy.py
@@ -0,0 +1,15 @@
+# pep8 -  formatowanie
+# snake_case - konwencja nazewnicza
+
+print("Hello World")
+print('Witaj Świecie')
+# blake8 - formater bardziej restrykcyjny
+
+# ctrl /  - komentarz
+# print('Hello world!")
+#   File "C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy-20-04-2026\day1\pierwszy.py", line 8
+#     print('Hello world!")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 8)
+# Process finished with exit code 1 bład w programie
+print("Dalsza część")
