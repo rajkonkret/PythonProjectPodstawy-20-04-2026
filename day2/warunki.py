9787450315d964a14c70c4c922a49c4a74c81e60
rajkonkret <rajkonkret660@gmail.com>
2026-04-21 14:40:59 +0200

    [2 34] update warunki.py

--- day2/warunki.py
+++ day2/warunki.py
@@ -56,3 +56,34 @@ if n > 3:
 # operator morsa, walrus operator
 if (n := len(a)) > 3:
     print(f"Długość wynosi: {n}, więcej niż 3")
+
+# ctrl /
+# podatek = 0
+# zarobki = int(input("Podaj zarobki:"))
+#
+# if zarobki < 10_000:
+#     podatek = 0
+# # elif zarobki >= 10_000 and zarobki < 40_000:
+# # elif 10_000 <= zarobki < 40_000:
+# elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+#
+# else:
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# dodac podatek 20%, dla zarobków od 10000 do 39999.99
+
+sum_zam = 150
+if sum_zam > 100:
+    rabat = 25
+else:
+    rabat = 0
+
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
+
+# operator warunkowy
+rabat = 25 if sum_zam > 100 else 0
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
