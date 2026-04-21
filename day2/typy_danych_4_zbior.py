4b70cd2ca344c21b069d0ff1e259e254646418b9
rajkonkret <rajkonkret660@gmail.com>
2026-04-21 11:45:45 +0200

    [2 17] update typy_danych_4_zbior.py

--- day2/typy_danych_4_zbior.py
+++ day2/typy_danych_4_zbior.py
@@ -34,3 +34,23 @@ zbior.add(24)
 zbior.add(25)
 
 print(zbior)
+# {33, 66, 777, 11, 44, 18, 22, 55, 24, 25}
+
+# usuniecie elemntu ze zbioru
+zbior.remove(55)  # wartość elementu
+print(zbior)  # {33, 66, 777, 11, 44, 18, 22, 24, 25}
+
+# pop()
+print(zbior.pop())  # 33, usunie pierwszy element
+
+zmienna = zbior.pop()
+print(f"Zmienna: {zmienna}")  # Zmienna: 66
+print('Zmienna:', zmienna)  # Zmienna: 66
+
+zbior_copy = zbior.copy()
+print(zbior_copy)  # {18, 22, 24, 777, 11, 44, 25}
+
+print(id(zbior))
+print(id(zbior_copy))
+# 2174014872864
+# 2174012117216
