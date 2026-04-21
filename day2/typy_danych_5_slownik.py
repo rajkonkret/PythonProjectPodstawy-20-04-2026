fdb3ffc54c6f3e313a48d613df0b7f4bfa1d7cfb
rajkonkret <rajkonkret660@gmail.com>
2026-04-21 12:27:53 +0200

    [2 23] update typy_danych_5_slownik.py

--- day2/typy_danych_5_slownik.py
+++ day2/typy_danych_5_slownik.py
@@ -43,3 +43,12 @@ print(dictionary['imie'])  # ['Radek', 'Tomek', 'Magda'] [1]
 print(dictionary['imie'][1])  # Tomek
 print(dictionary['imie'][1].lower())  # tomek
 print(dictionary['imie'][::-1])  # ['Magda', 'Tomek', 'Radek']
+
+dictionary_radek = {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+print(dictionary_radek)  # {'imie': ['Radek', 'Tomek', 'Magda'], 'wiek': 50}
+
+# print(dictionary_radek['Imie'])  # KeyError: 'Imie'
+print(dictionary['Imie'.lower()])  # ['Radek', 'Tomek', 'Magda']
+
+print(dictionary.get("Imie"))  # None
+print(dictionary.get("Imie", "default"))  # default
