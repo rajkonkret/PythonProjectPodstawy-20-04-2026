c4464ee4cf9259dc5e01a87af13508536514aa48
rajkonkret <rajkonkret660@gmail.com>
2026-04-21 11:53:30 +0200

    [2 19] update typy_danych_4_zbior.py

--- day2/typy_danych_4_zbior.py
+++ day2/typy_danych_4_zbior.py
@@ -65,3 +65,13 @@ print(type(zbior_2))  # <class 'set'>
 print(zbior | zbior_2)  # {777, 11, 44, 12.34, 18, 52, 22, 24, 25, 667, 62}
 print(zbior.union(zbior_2))  # {777, 11, 44, 12.34, 18, 52, 22, 24, 25, 667, 62}
 
+# część wspólna
+print(zbior & zbior_2)  # {18, 11, 44}
+print(zbior.intersection(zbior_2))  # {18, 11, 44}
+
+# różnica zbiorow
+print(zbior - zbior_2)  # {24, 777, 22, 25}
+print(zbior.difference(zbior_2))  # {24, 777, 22, 25}
+print(zbior_2.difference(zbior))  # {667, 52, 12.34, 62}
+
+
