7f7ae04763c7cd32f315d8bf232ba18bc3cdc07e
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 15:40:03 +0200

    [3 49] update csv_zad2.py

--- day3/csv_zad2.py
+++ day3/csv_zad2.py
@@ -1,12 +1,13 @@
 import csv
 
-filename = "records.csv"
+# filename = "records.csv"
+filename = "records_discount.csv"
 
 fields = []
 rows = []
 
 with open(filename, "r") as csv_f:
-    csvreader = csv.reader(csv_f)
+    csvreader = csv.reader(csv_f, delimiter=";")
 
     print(csvreader)  # <_csv.reader object at 0x000001F41DE67D60> iterator
 
@@ -19,3 +20,15 @@ print("Fields:", fields)
 print("Rows:", rows)
 # Fields: ['name', 'branch', 'year', 'cgpa']
 # Rows: [['radek', 'coe', '3', '0']]
+
+# Fields: ['sku;exp_date;price']
+# Rows: [['1;today;200'], ['2;today;100'], ['3;today;50'], ['4;today;200'], ['5;tomorrow;199.99'], ['6;tomorrow;2100'], ['7;today;1200']]
+
+# Fields: ['sku', 'exp_date', 'price']
+# Rows: [['1', 'today', '200'],
+# ['2', 'today', '100'],
+# ['3', 'today', '50'],
+# ['4', 'today', '200'],
+# ['5', 'tomorrow', '199.99'],
+# ['6', 'tomorrow', '2100'],
+# ['7', 'today', '1200']]
