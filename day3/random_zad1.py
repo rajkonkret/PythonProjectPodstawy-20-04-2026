bf8560efb9e98b4cc0bf302865993c8087d0d32f
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 10:06:08 +0200

    [3 10] update petle_zad1.py

--- day3/random_zad1.py
+++ day3/random_zad1.py
@@ -8,3 +8,7 @@ print(random.randint(1, 100))  # int, od 1 do 100
 
 print(random.randrange(1, 100))  # int, od 1 do 99
 print(random.randrange(5))  # od 0 do 4
+
+print(random.random())  # float, od 0 do 0.9999999 ->  0.600380112524912
+print(random.random() * 7)  # float, od 0 do 6.9999999 ->  1.5768172948473143
+
