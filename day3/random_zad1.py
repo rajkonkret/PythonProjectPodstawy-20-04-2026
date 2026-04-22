2b58cfe6c97c3a27534ddc18368884feb4ccda4d
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 10:03:43 +0200

    [3 9] update petle_zad1.py

---
+++ day3/random_zad1.py
@@ -0,0 +1,10 @@
+import random
+
+# działania na liczbach losowych
+
+#  """Return random integer in range [a, b], including both end points.
+#         """
+print(random.randint(1, 100))  # int, od 1 do 100
+
+print(random.randrange(1, 100))  # int, od 1 do 99
+print(random.randrange(5))  # od 0 do 4
