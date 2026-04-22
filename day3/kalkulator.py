3eac5d4a9e7366bc5be675d4e05e596c367a72c0
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 11:49:30 +0200

    [3 23] update kalkulator.py

--- day3/kalkulator.py
+++ day3/kalkulator.py
@@ -3,3 +3,22 @@
 # menu z opcjami
 # wyświetlić ładnie wynik 5 + 9 = 14
 # obsłużyć wyjątki
+while True:
+    print("""
+    1. Dodawanie
+    2. Odejmowanie
+    5. Koniec
+    """)
+
+    odp = input("Podaj wybraną opcję:")  # str
+
+    if odp == "5":
+        break
+
+    a = float(input("Podaj pierwszą liczbę"))
+    b = float(input("podaj drugą lizcbę"))
+
+    if odp == "1":
+        pass
+    elif odp == "2":
+        pass
