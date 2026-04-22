4febe0dda872ceb7bff12fe08cffc641c7539e6c
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2026-04-22 14:03:19 +0200

    Update pliki_zad1.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -3,4 +3,15 @@
 # with - context manager
 
 #
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
 with open("test.log", )
