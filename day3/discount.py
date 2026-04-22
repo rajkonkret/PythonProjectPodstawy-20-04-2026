a9bda114ed44b73b383a764c183dd61aa6d9a5d8
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 12:47:31 +0200

    [3 30] update discount.py

--- day3/discount.py
+++ day3/discount.py
@@ -14,9 +14,16 @@ print(today.year)  # 2026
 
 # foramtowanie daty
 formated_date = datetime.now().strftime("%d/%m/%Y")
-print(formated_date) # 22/04/2026
-print(type(formated_date)) # <class 'str'>
+print(formated_date)  # 22/04/2026
+print(type(formated_date))  # <class 'str'>
 
 # 11:16
 # 11:16:56
 # 11:16 am
+formated_time = datetime.now().strftime("%H:%M:%S")
+print(formated_time)  # 12:45:30
+print(type(formated_time))  # <class 'str'>
+
+formated_time_12h = datetime.now().strftime("%I:%M:%S %p")
+print(formated_time_12h)  # 12:46:22 PM
+print(type(formated_time_12h))  # <class 'str'>
