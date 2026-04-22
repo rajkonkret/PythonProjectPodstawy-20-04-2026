58a687a486ad4714b58f811c727f559f4fe6ddc6
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 14:08:54 +0200

    [3 37] update pliki_zad1.py

--- day3/pliki_zad1.py
+++ day3/pliki_zad1.py
@@ -14,4 +14,11 @@
 #     't'       text mode (default)
 #     '+'       open a disk file for updating (reading and writing)
 #     ========= ===============================================================
-with open("test.log", )
+
+# w - tworzy nowy plik
+# gdy plik istnieje zostania nadpisany (skasowany)
+with open("test.log", "w") as f:  # pod f dostaniemy ten filehandler
+    f.write("Powitanie\n")
+    f.write("Jeszcze jedno\n")
+
+# f.write("") # ValueError: I/O operation on closed file.
