2d705abb064c107e2e07d3928673ccb18a69b297
rajkonkret <rajkonkret660@gmail.com>
2026-04-22 10:19:59 +0200

    [3 11] update random_zad1.py

--- day3/random_zad1.py
+++ day3/random_zad1.py
@@ -1,4 +1,5 @@
 import random
+from calendar import prweek
 
 # działania na liczbach losowych
 
@@ -12,3 +13,14 @@ print(random.randrange(5))  # od 0 do 4
 print(random.random())  # float, od 0 do 0.9999999 ->  0.600380112524912
 print(random.random() * 7)  # float, od 0 do 6.9999999 ->  1.5768172948473143
 
+lista = [67, 45, 32, 68, 90, 42, 49]
+print(lista[random.randrange(len(lista))])  # liczba 49
+
+print(random.choice(lista))  # 49 element z listy, losuje jeden element
+
+for _ in range(6):
+    lista_kul = list(range(1, 50))
+    # print(lista_kul)
+    kula = random.choice(lista_kul)
+    lista_kul.remove(kula)
+    print(kula)
