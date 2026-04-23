67d265e6178f8a83b95da641ed91840df216d8b4
rajkonkret <rajkonkret660@gmail.com>
2026-04-23 09:36:19 +0200

    [4 5] update fun1

---
+++ day4/fun1.py
@@ -0,0 +1,15 @@
+# funkcja - wydzielony fragment kodu, można wykonać w dowolnym momencie
+# funkcja musi byc najpierw zadeklarowana
+# żeby funkcja się uruchomiła musimy ją wywołać
+
+a = 6
+b = 8
+
+
+# deklaracja funkcji
+def dodaj():  # funkcja bezargumentowa
+    print(a + b)
+
+
+# wywołanie funkcji
+dodaj()  # 14
