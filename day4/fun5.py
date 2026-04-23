0a5e9ae6d7d06052728cac069628b0d919c6f753
rajkonkret <rajkonkret660@gmail.com>
2026-04-23 11:22:16 +0200

    [4 14] update fun5

--- day4/fun5.py
+++ day4/fun5.py
@@ -2,3 +2,17 @@
 # ma miec dwie funkcje wewnętrzne: eur, usd
 # w zależności od parametru zwróci jedną z funkcji
 # przekazanie kwoty do funkcji usd, eur
+
+def kantor(waluta):
+    print("Otwieram kantor")
+
+    def usd():
+        pass
+
+    def eur():
+        pass
+
+    if waluta == "eur":
+        return eur
+    else:
+        return usd
