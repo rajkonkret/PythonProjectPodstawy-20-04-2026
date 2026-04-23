b8cc480f39d24565a27e5e203eb934f937b062b1
rajkonkret <rajkonkret660@gmail.com>
2026-04-23 14:26:27 +0200

    [4 32] update kl1

--- day4/kl1.py
+++ day4/kl1.py
@@ -7,4 +7,21 @@
 # __del__ - destruktor
 # paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
 
+# PascalCase, UpperCamelCase
+class Human:
+    # pass
+    """
+    Klasa Human opisująca człowieka w Pythonie
+    """
 
+
+# tworzenie obiektu
+cz1 = Human()
+print(Human.__doc__)  # Klasa Human opisująca człowieka w Pythonie
+
+# print(print.__doc__)
+# pydoc - narzędzie do dokumentacji
+# cd .\day4\ - wejscie do katalogu day4
+# cd .. - wyjscie do wyższego katalogu
+# pydoc -b - serwer dokumentacji
+# pydoc -w  kl1 - tworzy plik html z dokumentacją
