899849c581a2d22764d861d5132134e865d4e2a8
rajkonkret <rajkonkret660@gmail.com>
2026-04-23 12:32:55 +0200

    [4 21] update fun9

---
+++ day4/fun9.py
@@ -0,0 +1,31 @@
+# funkcja lambda
+# skrócony zapis funkcji
+# lambda zawsze zwraca wynik -> return
+# funkcja anonimowa
+
+def odejmij(a, b):
+    return a - b
+
+
+print(odejmij(10, 8))  # 2
+
+odejmij2 = lambda a, b: a - b
+print(odejmij2(10, 8))  # 2
+
+# zamien na lambda
+# def oblicz_vat(kwota, vat=23):
+#     return kwota * (100 + vat) / 100
+
+oblicz_vat = lambda kwota, vat=23: kwota * (100 + vat) / 100
+print(oblicz_vat(1000))
+print(oblicz_vat(1000, 8))
+# 1230.0
+# 1080.0
+
+wiek = lambda x: "dziecko" if x < 10 else ("nastolatek" if x < 18 else "dorosły")
+
+print(wiek(9))  # dziecko
+print(wiek(10))  # nastolatek
+print(wiek(17))  # nastolatek
+print(wiek(18))  # dorosły
+print(wiek(25))  # dorosły
