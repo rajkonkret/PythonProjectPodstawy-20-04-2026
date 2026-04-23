eabe84789b38d1af1d586d776ad954392aafc379
rajkonkret <rajkonkret660@gmail.com>
2026-04-23 14:11:37 +0200

    [4 31] update fun10

---
+++ day4/kl1.py
@@ -0,0 +1,10 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcja w klasie
+# obiekt - instancja klasy
+# klasa musi zostac najpierw zadeklarowana
+# tworzenie obiektu uruchamia funkcję inicjalizującą (konstruktor) __init__
+# __del__ - destruktor
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+
