fb659a850400a00fb232ed0298e3e8ec57dc66ed
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 11:58:04 +0200

    [5 19] update zad_zip.py

--- day5/zip_zad1.py
+++ day5/zip_zad1.py
@@ -1,2 +1,32 @@
+import time
+from itertools import zip_longest
+
 imiona = ["Radek", "Tomek", "Agata", "Marek", 'Asia']
 wiek = [44, 56, 23, 38]
+
+zipped = zip_longest(imiona, wiek, fillvalue=None)
+print(zipped)  # <itertools.zip_longest object at 0x000001BE8E6DE930>
+
+# for item in zipped:
+#     print(item)
+# ('Radek', 44)
+# ('Tomek', 56)
+# ('Agata', 23)
+# ('Marek', 38)
+# ('Asia', None)
+
+print(40 * "-")
+lista = []
+for o, w in zipped:
+    print(o, w)
+    time.sleep(1)
+    lista.append(o)
+# ----------------------------------------
+# ----------------------------------------
+# Radek 44
+# Tomek 56
+# Agata 23
+# Marek 38
+# Asia None
+
+print(lista)  # ['Radek', 'Tomek', 'Agata', 'Marek', 'Asia']
