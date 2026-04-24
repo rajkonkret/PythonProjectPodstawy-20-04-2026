a58973b6ef52a2b464592fcf4cff240e4713f991
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 09:54:21 +0200

    [5 8] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -39,6 +39,16 @@ class Kura(Ptak):
         print("ko ko ko ko ko")
 
 
+# Orzel(Ptak)
+class Orzel(Ptak):
+    """
+    Kalsa Orzel dziedziczy po klasie Ptak
+    """
+
+    def wydaj_odglos(self):
+        print("Kier kir ki kir")
+
+
 or1 = Ptak("Orzeł", 50)
 print(or1)  # <__main__.Ptak object at 0x000001A874D78980>, Orzeł, 50
 or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
@@ -46,3 +56,16 @@ or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
 kur1 = Ptak("Kura", 0)
 print(kur1)  # Kura, 0
 kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+
+kur2 = Kura("Kura zielononóżka")
+kur2.latam()
+kur2.wydaj_odglos()
+# Tu Kura Lecę z szybkością: 0 km/h
+# Tu Kura zielononóżka Ja nie latam.
+# ko ko ko ko ko
+
+or2 = Orzel("Orzeł Bielik", 55)
+or2.latam()
+or2.wydaj_odglos()
+# Tu Orzeł Bielik Lecę z szybkością: 55 km/h
+# Kier kir ki kir
