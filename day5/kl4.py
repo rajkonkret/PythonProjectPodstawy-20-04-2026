296cb85e73fbcdaefecd6fa834a5967b42afb324
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 09:07:04 +0200

    [5 1] update kl4

---
+++ day5/kl4.py
@@ -0,0 +1,14 @@
+class Ptak:
+    """
+    Klasa opsująca ptaka w Pythonie
+    """
+
+    def __init__(self, gatunek, szybkosc):
+        """
+        Meto inicjalizująca - konstruktor
+        :param gatunek:
+        :param szybkosc:
+        """
+
+        self.gatunek = gatunek
+        self.szybkosc = szybkosc
