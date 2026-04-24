80de833da092d660be15861e6df6caac9bcbf946
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 09:24:33 +0200

    [5 6] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -24,3 +24,7 @@ class Ptak:
 or1 = Ptak("Orzeł", 50)
 print(or1)  # <__main__.Ptak object at 0x000001A874D78980>, Orzeł, 50
 or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+
+kur1 = Ptak("Kura", 0)
+print(kur1)  # Kura, 0
+kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
