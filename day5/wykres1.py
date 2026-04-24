93be4d85b3c1a725a81993078b3131f78908c709
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 15:34:44 +0200

    [5 42] update arkusz2.py

--- day5/wykres1.py
+++ day5/wykres1.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [2, 5, 7, 9, 11]
 
-plt.plot(x, y)
+# plt.plot(x, y)
+plt.scatter(x, y, c="red")
+plt.title("Wykres punktowy")
 
+plt.xlabel("Oś X")
+plt.ylabel("Oś Y")
+
+plt.savefig("wykres.png")
 plt.show()
