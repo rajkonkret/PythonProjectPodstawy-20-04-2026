90759b018b83898da5ad647a2be71f81307a58bd
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 14:14:51 +0200

    [5 29] update baza2.py

--- day5/baza2.py
+++ day5/baza2.py
@@ -12,26 +12,45 @@ try:
     print("Baza  danych zostałą podłaczona")
 
     query = """
-    CREATE TABLE IF NOT EXISTS developers(
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    salary REAL NOT NULL
-    );
-    """
+            CREATE TABLE IF NOT EXISTS developers
+            (
+                id
+                INTEGER
+                PRIMARY
+                KEY,
+                name
+                TEXT
+                NOT
+                NULL,
+                email
+                TEXT
+                NOT
+                NULL
+                UNIQUE,
+                salary
+                REAL
+                NOT
+                NULL
+            ); \
+            """
 
     c.execute(query)
     conn.commit()
 
     # insert
-    insert = "INSERT INTO developers (id,name,email,salary) VALUES (1,'Radek', 'raj@raj.pl', 10000);"
-    c.execute(insert)
-    conn.commit()
+    # insert = "INSERT INTO developers (id,name,email,salary) VALUES (1,'Radek', 'raj@raj.pl', 10000);"
+    # c.execute(insert)
+    # conn.commit()
 
-    insert = "INSERT INTO developers (id,name,email,salary) VALUES (2,'Radek', 'raj1@raj.pl', 10000);"
-    c.execute(insert)
-    conn.commit()
+    # insert = "INSERT INTO developers (id,name,email,salary) VALUES (2,'Radek', 'raj1@raj.pl', 10000);"
+    # c.execute(insert)
+    # conn.commit()
 
+    select = "SELECT * FROM developers;"
+    for row in c.execute(select):
+        print(row)
+    # (1, 'Radek', 'raj@raj.pl', 10000.0)
+    # (2, 'Radek', 'raj1@raj.pl', 10000.0)
 except sqlite3.Error as e:
     print("Bład podłaczenia danych:", e)
 finally:
