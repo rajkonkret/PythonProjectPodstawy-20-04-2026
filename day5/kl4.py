fef30d5cb8341011180be650a32292aff58fa357
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 10:13:55 +0200

    [5 11] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
 
+from day1.pierwszy import name
+
 
 # klasa abstrakcyjna
 # posiada metodę abstrakcyjna
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
 class Ptak(ABC):
     """
     Klasa opsująca ptaka w Pythonie
@@ -56,13 +59,21 @@ class Orzel(Ptak):
         print("Kier kir ki kir")
 
 
-or1 = Ptak("Orzeł", 50)
-print(or1)  # <__main__.Ptak object at 0x000001A874D78980>, Orzeł, 50
-or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+class Sowa(Ptak):
+    """
+    Klasa Sowa, dziedziczy po Ptak
+    """
 
-kur1 = Ptak("Kura", 0)
-print(kur1)  # Kura, 0
-kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
+
+# klasa abstrakcyjna - nie można tworzyc obiektów klasy Ptak
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
+# or1 = Ptak("Orzeł", 50)
+# print(or1)  # <__main__.Ptak object at 0x000001A874D78980>, Orzeł, 50
+# or1.latam()  # Tu Orzeł Lecę z szybkością: 50 km/h
+#
+# kur1 = Ptak("Kura", 0)
+# print(kur1)  # Kura, 0
+# kur1.latam()  # Tu Kura Lecę z szybkością: 0 km/h
 
 kur2 = Kura("Kura zielononóżka")
 kur2.latam()
@@ -76,3 +87,17 @@ or2.latam()
 or2.wydaj_odglos()
 # Tu Orzeł Bielik Lecę z szybkością: 55 km/h
 # Kier kir ki kir
+
+# polimorfizm - obiekty róznych klas maja wspolne metody
+lista = [or2, kur2]  # obiekty różnych klas
+
+for i in lista:
+    i.wydaj_odglos()
+    print(i.__class__.__name__)
+# Kier kir ki kir
+# Orzel
+# ko ko ko ko ko
+# Kura
+
+# TypeError: Can't instantiate abstract class Sowa without an implementation for abstract method 'wydaj_odglos'
+# sowa = Sowa("Sowa", 10)
