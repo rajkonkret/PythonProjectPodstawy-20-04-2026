7fff4e0e2f62631934f3c52ae548cc9c2ae34bc6
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 12:22:46 +0200

    [5 21] update api_nbp.py

---
+++ day5/api_nbp.py
@@ -0,0 +1,5 @@
+# REST API (Representational State Transfer) to
+# elastyczny, bezstanowy styl architektury oprogramowania wykorzystujący protokół HTTP do wymiany danych między systemami.
+# https://github.com/public-apis/public-apis
+
+url = 'https://api.nbp.pl/api/exchangerates/rates/A/USD/'
