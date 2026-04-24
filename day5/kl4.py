110b6ed10da323ff7e57abf348710682fb52a010
rajkonkret <rajkonkret660@gmail.com>
2026-04-24 09:18:11 +0200

    [5 4] update kl4

--- day5/kl4.py
+++ day5/kl4.py
@@ -17,7 +17,10 @@ class Ptak:
         print("Tu", self.gatunek, "Lecę z szybkością:", self.szybkosc, "km/h")
 
     # __str__
+    def __str__(self):
+        return f"{self.gatunek}, {self.szybkosc}"
 
 
 or1 = Ptak("Orzeł", 50)
-print(or1) # <__main__.Ptak object at 0x000001A874D78980>
+print(or1)  # <__main__.Ptak object at 0x000001A874D78980>, Orzeł, 50
+
